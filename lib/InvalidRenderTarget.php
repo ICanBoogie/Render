ef1c8bdca09186dbfc2cf8d9209f9ee5b2dfd72f
Olivier Laviale <olivier.laviale@gmail.com>
2015-12-27 04:24:13 +0100

    Refactor, added invalidRenderTarget

---
+++ lib/InvalidRenderTarget.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ICanBoogie\Render;
+
+use ICanBoogie\Accessor\AccessorTrait;
+
+/**
+ * Exception thrown when the target to render is invalid.
+ *
+ * @property-read mixed $target
+ */
+class InvalidRenderTarget extends \InvalidArgumentException implements Exception
+{
+	use AccessorTrait;
+
+	/**
+	 * @var mixed
+	 */
+	private $target;
+
+	/**
+	 * InvalidRenderTarget constructor.
+	 *
+	 * @param mixed $target
+	 * @param string $message
+	 * @param int $code
+	 * @param \Exception|null $previous
+	 */
+	public function __construct($target, $message = null, $code = 500, \Exception $previous = null)
+	{
+		$this->target = $target;
+
+		parent::__construct($message ?: $this->format_message($target), $code, $previous);
+	}
+
+	/**
+	 * Format exception message.
+	 *
+	 * @param mixed $target
+	 *
+	 * @return string
+	 */
+	protected function format_message($target)
+	{
+		$type = gettype($target);
+
+		return "Invalid render target. Expected object or array, got: $type (`$target`)";
+	}
+}
--- lib/Renderer.php
+++ lib/Renderer.php
@@ -16,6 +16,12 @@ namespace ICanBoogie\Render;
  */
 class Renderer
 {
+	const OPTION_LAYOUT = 'layout';
+	const OPTION_PARTIAL = 'partial';
+	const OPTION_TEMPLATE = 'template';
+	const OPTION_CONTENT = 'content';
+	const OPTION_LOCALS = 'locals';
+
 	/**
 	 * @var TemplateResolver
 	 */
@@ -53,48 +59,88 @@ class Renderer
 	 */
 	public function render($target_or_options, array $additional_options = [])
 	{
-		$this->template_resolver = clone $this->original_template_resolver;
+		if (!$target_or_options)
+		{
+			return null;
+		}
+
 		$options = $this->resolve_options($target_or_options, $additional_options);
+		$content = $options[self::OPTION_CONTENT];
+		$variables = $options[self::OPTION_LOCALS];
 
-		if (isset($options['partial']))
+		$this->template_resolver = clone $this->original_template_resolver;
+
+		#
+
+		$template = $options[self::OPTION_PARTIAL];
+
+		if ($template)
 		{
-			$tried = [];
-			$template = $this->resolve_template_name($options['partial'])->as_partial;
-			$template_pathname = $this->template_resolver->resolve($template, $this->engines->extensions, $tried);
-
-			if (!$template_pathname)
-			{
-				throw new TemplateNotFound("There is no partial matching <q>$template</q>.", $tried);
-			}
-
-			return $this->engines->render($template_pathname, null, $options['locals']);
+			$content = $this->render_partial($template, $variables);
 		}
 
-		$tried = [];
-		$template = $options['template'];
-		$template_pathname = $this->template_resolver->resolve($template, $this->engines->extensions, $tried);
+		$template = $options[self::OPTION_TEMPLATE];
 
-		if (!$template_pathname)
+		if ($template)
 		{
-			throw new TemplateNotFound("There is no template matching <q>$template</q>.", $tried);
+			$content = $this->render_template($template, 'template', $content, $variables);
 		}
 
-		$rc = $this->engines->render($template_pathname, $options['content'], $options['locals']);
+		$template = $options[self::OPTION_LAYOUT];
 
-		if (isset($options['layout']))
+		if ($template)
 		{
-			$template = $this->resolve_template_name($options['layout'])->as_layout;
-			$template_pathname = $this->template_resolver->resolve($template, $this->engines->extensions, $tried);
-
-			if (!$template_pathname)
-			{
-				throw new TemplateNotFound("There is no partial matching <q>$template</q>.", $tried);
-			}
-
-			$rc = $this->engines->render($template_pathname, null, [ 'content' => $rc ] + $options['locals']);
+			$content = $this->render_layout($template, [ self::OPTION_CONTENT => $content ] + $variables);
 		}
 
-		return $rc;
+		return $content;
+	}
+
+	/**
+	 * Renders partial.
+	 *
+	 * @param string $template
+	 * @param array $variables
+	 *
+	 * @return string
+	 */
+	protected function render_partial($template, $variables)
+	{
+		$template = $this->resolve_template_name($template)->as_partial;
+
+		return $this->render_template($template, 'partial', null, $variables);
+	}
+
+	/**
+	 * Renders layout.
+	 *
+	 * @param string $template
+	 * @param array $variables
+	 *
+	 * @return string
+	 */
+	protected function render_layout($template, array $variables)
+	{
+		$template = $this->resolve_template_name($template)->as_layout;
+
+		return $this->render_template($template, 'layout', null, $variables);
+	}
+
+	/**
+	 * Renders template.
+	 *
+	 * @param string $template
+	 * @param string $type
+	 * @param string $content
+	 * @param array $variables
+	 *
+	 * @return string
+	 */
+	protected function render_template($template, $type, $content, $variables)
+	{
+		$template_pathname = $this->resolve_template_pathname($template, $type);
+
+		return $this->engines->render($template_pathname, $content, $variables);
 	}
 
 	/**
@@ -104,6 +150,8 @@ class Renderer
 	 * @param array $additional_options
 	 *
 	 * @return array
+	 *
+	 * @throws InvalidRenderTarget if rendering target is invalid.
 	 */
 	protected function resolve_options($target_or_options, array $additional_options = [])
 	{
@@ -111,20 +159,32 @@ class Renderer
 
 		if (is_object($target_or_options))
 		{
-			$additional_options['content'] = $target_or_options;
-			$additional_options['locals']['this'] = $target_or_options;
+			$additional_options[self::OPTION_CONTENT] = $target_or_options;
+			$additional_options[self::OPTION_LOCALS]['this'] = $target_or_options;
 
-			if (empty($additional_options['partial']) && empty($additional_options['template']))
+			if (empty($additional_options[self::OPTION_PARTIAL]) && empty($additional_options[self::OPTION_TEMPLATE]))
 			{
-				$additional_options['partial'] = $this->resolve_template_name($target_or_options);
+				$additional_options[self::OPTION_PARTIAL] = $this->resolve_template_name($target_or_options);
 			}
 		}
 		else if (is_array($target_or_options))
 		{
 			$options = $target_or_options;
 		}
+		else
+		{
+			throw new InvalidRenderTarget($target_or_options);
+		}
 
-		return $additional_options + $options;
+		return $additional_options + $options + [
+
+			self::OPTION_CONTENT => null,
+			self::OPTION_PARTIAL => null,
+			self::OPTION_TEMPLATE => null,
+			self::OPTION_LAYOUT => null,
+			self::OPTION_LOCALS => []
+
+		];
 	}
 
 	/**
@@ -137,5 +197,25 @@ class Renderer
 	protected function resolve_template_name($content)
 	{
 		return TemplateName::from($content);
+	}
+
+	/**
+	 * @param string $template
+	 * @param string $type One of "template", "partial", "layout".
+	 *
+	 * @return string Template pathname.
+	 *
+	 * @throws TemplateNotFound if the template pathname cannot be resolved.
+	 */
+	protected function resolve_template_pathname($template, $type)
+	{
+		$template_pathname = $this->template_resolver->resolve($template, $this->engines->extensions, $tried);
+
+		if (!$template_pathname)
+		{
+			throw new TemplateNotFound("There is no $type matching `$template`.", $tried);
+		}
+
+		return $template_pathname;
 	}
 }
